b81f790e73654483afef20613f9191fb2f7a0736
Divya Parmar <divyaparmar0207@gmail.com>
2024-02-26 10:25:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/differentpixeldensities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.differentpixeldensities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+        supportActionBar!!.setLogo(R.mipmap.logo_image)
+        supportActionBar!!.setDisplayUseLogoEnabled(true)
+
+        setContentView(R.layout.activity_main)
+    }
+}
